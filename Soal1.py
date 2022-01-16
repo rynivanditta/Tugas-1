d114727a4f74f7b2131bfd14deff73878e97102f
unknown <ryni.vd@gmail.com>
2022-01-16 14:53:28 +0300

    Tugas 1

---
+++ Soal1.py
@@ -0,0 +1,6 @@
+#Membuat tampilan di layar dengan format : Nama saya [Nama], umur saya [Umur] tahun dan tinggi saya [Tinggi] cm.
+Nama = str(input("Masukan nama anda :"))
+Umur = int(input("Masukan umur anda  :"))
+Tinggi = float(input("Masukan tinggi anda  :"))
+txt=f"Nama saya adalah {Nama}, umur saya {Umur} tahun, tinggi saya {Tinggi} cm"
+print(txt)
